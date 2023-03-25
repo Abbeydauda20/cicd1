6df6196d38e1137081e3be521ad457677f7348c2
Abbey <abbeydauda20@gmail.com>
2023-03-25 16:34:25 -0400

    New changes

--- main.tf
+++ main.tf
@@ -1,13 +1,34 @@
+# Pull ami ID based on filters
+data "aws_ami" "linux2" {
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20230207.0-x86_64-gp2"]
+
+  }
+
+  owners = ["amazon"]
+}
+
+
 
 #Creating ec2 instance
 
 resource "aws_instance" "ec2_example" {
-    ami = "ami-02f3f602d23f1659d" 
+    ami = data.aws_ami.linux2.id
     instance_type = "t3.nano"
     tags = {
       Name ="${terraform.workspace}-instance"
     }
 }
+
+
 
 # Creating VPC
 resource "aws_vpc" "demovpc" {
